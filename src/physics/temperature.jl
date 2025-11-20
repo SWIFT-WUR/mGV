db8b596e42619a5625e95e1d89db07aa9b41d82e
Karun <karun.datadien@wur.nl>
2025-11-20 09:27:22 +0100

    [ISSUE #9] reduce max iterations in temperature solver

--- src/physics/temperature.jl
+++ src/physics/temperature.jl
@@ -71,7 +71,7 @@ function solve_surface_temperature(tsurf, soil_temperature, albedo, Rs, RL, rh, 
     Ts_new  = tsurf
 
     tolerance = 1e-3
-    max_iter  = 20
+    max_iter  = 3
 
     for iter in 1:max_iter
         residual   = f(Ts_new, Ts_old)   # Ts_old stays fixed = previous time step
