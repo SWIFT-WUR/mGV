808f286be1b98078751b6552a70ce53cefaee78f
Karun <karun.datadien@wur.nl>
2025-11-26 10:45:12 +0100

    Revert "[ISSUE #12] try safe copyto with streaming"
    This reverts commit 2970b4e99b0da11614aadd73c0530f20a55e7f54.

--- src/io/netcdf_writer.jl
+++ src/io/netcdf_writer.jl
@@ -318,10 +318,7 @@ function async_transfer!(
     function async_copy(dest, src)
         # We use unsafe_copyto! to bypass Julia's default blocking mechanisms
         # The stream argument tells CUDA to queue this and move on.
-        #CUDA.unsafe_copyto!(pointer(dest), pointer(src), length(dest), stream=stream)
-        CUDA.stream!(stream) do
-            copyto!(dest, src)
-        end
+        CUDA.unsafe_copyto!(pointer(dest), pointer(src), length(dest), stream=stream)
     end
 
     # --- 2D Raw ---
