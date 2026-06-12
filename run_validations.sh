03f995a9e150d38cc7ba142d67d8c64dd60483c1
Karun <karun.datadien@wur.nl>
2026-06-12 12:14:55 +0200

    [ISSUE #26] only run mekong testcase since indus data not available to CI

--- run_validations.sh
+++ run_validations.sh
@@ -1,5 +1,5 @@
 julia --project=. run.jl configs/mekong_config.toml
-julia --project=. run.jl configs/indus_config.toml
+#julia --project=. run.jl configs/indus_config.toml
 
 cd ./validations
 python plot_dashboards_mekong_indus.py
