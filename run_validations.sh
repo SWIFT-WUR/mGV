2c3542f3acc6f08a69a802991631dbedbb5151c0
Karun <karun.datadien@wur.nl>
2026-06-12 14:23:25 +0200

    [ISSUE #26] change plotscript call to just mekong for CI

--- run_validations.sh
+++ run_validations.sh
@@ -2,4 +2,5 @@ julia --project=. run.jl configs/mekong_config.toml 1979 1980
 #julia --project=. run.jl configs/indus_config.toml
 
 cd ./validations
-python plot_dashboards_mekong_indus.py
+#python plot_dashboards_mekong_indus.py
+python plot_dashboards_mekong.py
