80c0167436fd6f048975c67e2960d186163f63b3
Karun <karun.datadien@wur.nl>
2026-06-12 11:55:01 +0200

    [ISSUE #26] using new cmndline arguments to run

--- run_validations.sh
+++ run_validations.sh
@@ -1,5 +1,5 @@
-julia --project=. run.jl mekong 1979 1980
-julia --project=. run.jl indus 1979 1980
+julia --project=. run.jl configs/mekong_config.toml
+julia --project=. run.jl configs/indus_config.toml
 
 cd ./validations
 python plot_dashboards_mekong_indus.py
