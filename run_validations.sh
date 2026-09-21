a6f63594d23e2627ccf05f1fc95c23c484a26132
KarunDatadien <karun.datadien@wur.nl>
2026-09-21 12:41:33 +0200

    Fixes to CI

--- run_validations.sh
+++ run_validations.sh
@@ -1,6 +1,7 @@
 set -e
 
 julia --project=. -e 'using mGV; mGV.run()' -- configs/mekong_config.toml
-julia --project=. -e 'using mGV; mGV.run()' -- configs/indus_config.toml
+# Indus input data is not yet in the repo
+# julia --project=. -e 'using mGV; mGV.run()' -- configs/indus_config.toml
 
 python3 validations/plot_dashboard.py mekong
